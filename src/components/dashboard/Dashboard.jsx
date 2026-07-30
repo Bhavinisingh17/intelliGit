9571488e402f267ecdabd0b400006da00477a774
Bhavini Singh <bhavinisingh017@gmail.com>
2026-07-31 03:04:02 +0530

    Improve UI

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,41 +1,43 @@
-import React from 'react'
+import React from "react";
+import heroImg from "../../assets/hero.png";
 
 function Dashboard() {
   return (
     <div>
+      <header className="w-full bg-[#E7D7BB] rounded-lg px-6 md:px-10 py-8">
+        <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-8">
 
-<header
-  className="
-    w-full
-    h-64
-    bg-[#E7D7BB]
-    !pl-20
-    sm:px-6
-    md:px-8
-     flex
-    flex-col
-    justify-center
-    rounded-lg
-  "
->
-    <p>Welcome to intelligit</p>
-    <h1
-    
-   className="
+          {/* Left Content */}
+          <div className="w-full md:w-1/2">
+            <p className="!ml-5 text-sm md:text-base font-bold">
+              Welcome to IntelliGit
+            </p>
 
-    text-xl
-    sm:text-4xl
-    md:text-5xl
-    font-bold
-    tracking-tight
-    text-[#2F241D]
-  "
-  
->Analyze Github Profiles <br></br>like never before</h1>
-</header>
+            <h1 className="!ml-5 !mt-3 text-xl sm:text-4xl lg:text-5xl font-bold text-[#2F241D] max-[622px]:mt-1">
+              Analyze GitHub Profiles
+              <br />
+              like never before.
+            </h1>
 
+<p className="!ml-5 !mt-3 text-sm sm:text-base sm:mt-2 text-gray-700 max-[622px]:mt-1"> Search any public GitHub profile to explore
+               repositories,
+              languages, stars, forks, and much more.
+            </p>
+          </div>
+
+          {/* Right Image */}
+          <div className="w-full md:w-1/2 flex justify-center">
+            <img
+              src={heroImg}
+              alt="Hero"
+              className="w-64 sm:w-80 md:w-[380px] lg:w-[450px] h-auto"
+            />
+          </div>
+
+        </div>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
